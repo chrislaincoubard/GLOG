5821f5296f589f0debc608510fdc659ecbca95f6
chrislaincoubard <101561839+chrislaincoubard@users.noreply.github.com>
2022-11-21 14:16:25 +0100

    minor changes

--- app(1).R
+++ app(1).R
@@ -5,20 +5,21 @@ ui <- fluidPage(
   
   sidebarLayout(
     sidebarPanel(
-      helpText("Choose a file first"),
+      helpText("Upload a file, choose the test you want to perform and then choose the column for the chosen test"),
       
-      radioButtons("col_choice", "Input radio buttons",
-                   c("Mean", "Liste", "des", "tests", "stats"), selected = character(0)),
+      radioButtons("test_choice", "Input radio buttons",
+                   c("Mean", "None"), selected = character(0)),
       
       sliderInput("range", 
                   label = "Range of interest:",
                   min = 0, max = 100, value = c(0, 100)),
+      checkboxInput("show", "show_dataframe"),
       
-      checkboxGroupInput("col_choice_2", "choose the column",
+      checkboxGroupInput("column_choice", "choose the column",
                          choices = c("Choose a file first")),
       
       fileInput("filechoser", label = "filechoser", accept = ".csv"),
-      checkboxInput("header", "HEADER", TRUE),
+      
     ),
     
     
@@ -35,18 +36,18 @@ ui <- fluidPage(
 server <- function(input, output,session) {
   
   observe({
-    updateCheckboxGroupInput(session, "col_choice_2", 
+    updateCheckboxGroupInput(session, "column_choice", 
                              label = "choose the column(s)",
                              choices = colnames(read_file()))
-    print(colnames(read_file()))
   })
+  
   update_list <- reactive({
     choice_list = colnames(read_file())
     choice_list
   })
   
   selected <- reactive({
-    curr_selected <- input$col_choice_2
+    curr_selected <- input$column_choice
     curr_selected
   })
   
@@ -54,41 +55,43 @@ server <- function(input, output,session) {
     inFile <- input$filechoser
     if (is.null(inFile))
       return(NULL)
-    df <- read.csv(inFile$datapath, header = TRUE)  
+    df <- read.csv(inFile$datapath, header = TRUE, sep = ";")  
     return(df)
   })
   
   
   output$contents <- renderDataTable({
-    df <- read_file()
-    head(df[input$col_choice_2], 10)
+    if (input$show) {
+      df <- read_file()
+      return (df)
+    }
+    NULL
   })
   
   output$selected_var <- renderText({
-    paste("You have selected this", input$col_choice)
+    if (length(input$column_choice)){
+      paste("You have selected this", input$column_choice)
+    }
+    else {
+      paste("")
+    }
   })
-  
-  # output$mean <- renderText ({
-  #   data = read_file()
-  #   choice = input$var
-  #   print(choice)
-  #   if (is.null(data)){
-  #     paste("You still have not imported files")
-  #     return (NULL)
-  #   }
-  #   moy = mean(data[,as.integer(choice)])
-  #   paste("The mean is : ", moy)
-  # })
+
   output$Mean <- renderText ({
     
     final_msg = ""
-    if (input$col_choice == "Mean"){
-      df = read_file()
-      for (value in unlist(input$col_choice_2)){
-      moy = mean(as.numeric(unlist(na.omit(df[value]))))
-      final_msg = paste(final_msg, "La moyenne est de", moy, "\n")
+    if (length(input$test_choice) | input$test_choice == "None"){
+      if (input$test_choice == "Mean"){
+        df = read_file()
+        if (length(input$column_choice)){
+          for (value in unlist(input$column_choice)){
+            moy = mean(as.numeric(unlist(na.omit(df[value]))))
+            final_msg = paste(final_msg, "La moyenne est de", moy, "\n")
+          }
+        }
+        
+      }
     }
-  }
     
     paste(final_msg, sep = "\n")
     
