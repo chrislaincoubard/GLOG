4b5f615692413ab7d7ea1821c7840d56412f5651
Elena Nicollin <elena.nicollin@gmail.com>
2022-11-25 17:05:08 +0100

    Affichage carte fonctionnel

--- app.R
+++ app.R
@@ -1,14 +1,16 @@
+# webshot::install_phantomjs()
 library(colourpicker)
 library(data.table)
 library(ggplot2)
+library(maps)
+library(mapview)
 library(RColorBrewer)
 library(shiny)
-#library(tmap)
+library(tmap)
 
 source("maps.R")
 
-world_data = map_data("world")
-world_data$ISO2 = iso.alpha(world_data$region,2)
+data("World")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -27,12 +29,14 @@ ui <- fluidPage(
           selectInput("col_choice", "Choose the type of data to visualise:",
                       choices = c("Select file first")),
           
-          colourInput("col", "Select colour", value = "ivory")
+          colourInput("color1", "Select first color for gradient", value = "#DEF4FF"),
+          colourInput("color2", "Select last color for gradient", value = "#0C4999")
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-          plotOutput("mapView"),
+          tmapOutput("mapView"),
+          downloadButton("downloadMap"),
           dataTableOutput("contents")
         )
     )
@@ -47,14 +51,12 @@ server <- function(input, output, session) {
     updateSelectInput(session, "country_col", 
                       label = "Indicate the column containing the country names:",
                       choices = colnames(read_file()))
-    print(colnames(read_file()))
   })
   
   observe({
     updateSelectInput(session, "col_choice", 
                              label = "Choose the type of data to visualise:",
                              choices = colnames(read_file()))
-    print(colnames(read_file()))
   })
   
   update_list <- reactive({
@@ -72,24 +74,46 @@ server <- function(input, output, session) {
     col
   })
   
+  col1 <- reactive({
+    input$color1
+  })
+  
+  col2 <- reactive({
+    input$color2
+  })
+  
   read_file <- reactive({
     inFile <- input$filebrowse
     if (is.null(inFile))
       return(NULL)
-    df <- fread(inFile$datapath)
+    df <- read.csv(inFile$datapath, header=T, sep=";", fileEncoding="UTF-8-BOM")
     return(df)
   })
   
-  # output$summary <- renderPrint({
-  #   df <- read_file()
-  #   paste(cat("File summary:\n"),summary(df))
-  # })
-  
-  output$mapView <- renderPlot({
+  current_map <- reactive({
     df <- read_file()
-    data_type <- selected
-    worldMaps(df, world_data, data_type)
+    if (is.null(df)){
+      p = plot_empty_map()
+    }
+    else{
+      country_col <- countries()
+      data_type <- selected()
+      color1 <- col1()
+      color2 <- col2()
+      p = plot_map(df, country_col, data_type, color1, color2)
+    }
   })
+  
+  output$mapView <- renderTmap({
+    current_map()
+  })
+  
+  output$downloadMap <- downloadHandler(
+    filename = "monkeypox_map.png",
+    content = function(file) {
+      mapshot(tmap_leaflet(current_map()), file = file)
+    }
+  )
   
   output$contents <- renderDataTable({
     df <- read_file()
--- maps.R
+++ maps.R
@@ -1,79 +1,27 @@
-#rm(list=ls())
+#Map 
+plot_map <- function(df, country_col, data_type, color1, color2){
+  tmap_mode("view")
+  
+  df$iso_a3=iso.alpha(df[,country_col],3)
+  joined_df = merge(World, df, by="iso_a3")
+  
+  pal <- colorRampPalette(c(color1, color2))
 
-test = function(){
-  library(ggplot2)
-  library(tidyverse)
+  p = tm_shape(joined_df) +
+    tm_polygons(col=data_type, palette=pal(2), style="cont", popup.vars=c(country_col, data_type)) +
+    tm_view(set.view = c(10,30,1.5))
   
-  setwd("C:/Users/Elena/Desktop/Mes Documents/M2/GLOG/projet")
-  
-  data = read.csv("data/sample2.csv", sep=";", header=T, fileEncoding = "UTF-8-BOM")
-  mapdata = map_data("world")
-  mapdata = left_join(mapdata, data, by="region")
-  mapdata = mapdata[!is.na(mapdata$cases),]
-  
-  mapdata_europe = mapdata[mapdata$long>-21 & mapdata$long<35 & mapdata$lat>34 & mapdata$lat<71,]
-  
-  map1=ggplot(mapdata_europe, aes(x=long, y=lat, group=group))+
-    geom_polygon(aes(fill=cases),color="black")
-  map1
-  
-  map2 = map1 + scale_fill_gradient(name="#cases", low="ivory", high="red", na.value="grey50")+
-    theme(axis.text.x=element_blank(),
-          axis.text.y=element_blank(),
-          axis.ticks=element_blank(),
-          axis.title.y=element_blank(),
-          axis.title.x=element_blank(),
-          rect=element_blank())
-  map2
-  
-  mapdata[mapdata$region=="Russia",]
-  
-  #-21, 25 / 68, 34
-  
+  return(p)
 }
 
 
-worldMaps <- function(df, world_data, data_type, period=NULL, indicator=NULL){
-  
-  # Function for setting the aesthetics of the plot
-  my_theme <- function () { 
-    theme_bw() + theme(axis.text = element_text(size = 14),
-                       axis.title = element_text(size = 14),
-                       strip.text = element_text(size = 14),
-                       panel.grid.major = element_blank(), 
-                       panel.grid.minor = element_blank(),
-                       panel.background = element_blank(), 
-                       legend.position = "bottom",
-                       panel.border = element_blank(), 
-                       strip.background = element_rect(fill = 'white', colour = 'white'))
-  }
-  
-  # Select only the data that the user has selected to view
-  plotdf <- df[df$Indicator == indicator & df$DataType == data_type & df$Period == period,]
-  plotdf <- plotdf[!is.na(plotdf$ISO3), ]
-  plotdf <- df[]
-  
-  # Add the data the user wants to see to the geographical world data
-  world_data['DataType'] <- rep(data_type, nrow(world_data))
-  world_data['Period'] <- rep(period, nrow(world_data))
-  world_data['Indicator'] <- rep(indicator, nrow(world_data))
-  world_data['Value'] <- plotdf$Value[match(world_data$ISO3, plotdf$ISO3)]
-  
-  # Create caption with the data source to show underneath the map
-  capt <- paste0("Source: ", ifelse(data_type == "Childlessness", "United Nations" , "World Bank"))
-  
-  # Specify the plot for the world map
-  library(RColorBrewer)
-  library(ggiraph)
-  g <- ggplot() + 
-    geom_polygon_interactive(data = world_data, color = 'gray70', size = 0.1,
-                             aes(x = long, y = lat, fill = Value, group = group, 
-                                 tooltip = sprintf("%s<br/>%s", ISO3, Value))) + 
-    scale_fill_gradientn(colours = brewer.pal(5, "RdBu"), na.value = 'white') + 
-    scale_y_continuous(limits = c(-60, 90), breaks = c()) + 
-    scale_x_continuous(breaks = c()) + 
-    labs(fill = data_type, color = data_type, title = NULL, x = NULL, y = NULL, caption = capt) + 
-    my_theme()
-  
-  return(g)
+# Map displayed when no file is selected (world basemap + grey countries and borders)
+plot_empty_map <- function(){
+  tmap_mode("view")
+   p = tm_shape(World) +
+     tm_fill(col = "#eaeaea") +
+     tm_borders(col = "#d0cfd4") +
+     tm_view(set.view = c(10,30,1.5))
+    
+   return(p)
 }
